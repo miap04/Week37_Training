e49dd63ddb3b54b2ac1e3558cc9fc6d965c49ac1
miap04 <miapederse@gmail.com>
2024-09-09 17:05:17 +0200

    Create week37.js

---
+++ week37.js
@@ -0,0 +1,79 @@
+
+*/
+console.log("Task: Example");
+const people = ["Tony", "Christian", "Håkon"]
+
+for (let index = 0; index < people.length; index++) {
+    let person = people[index];
+    console.log(person);
+}
+
+
+console.log("Task: A");
+
+for (let i = 0; i < 100; i++) {
+    console.log("Debugging is like being a detective in a crime drama where you are also the murderer");
+  }
+
+
+
+console.log("Task: B");
+
+const max = 100;
+for (let index = 1; index <= max; index++) {
+    console.log(index + " bottles of soda on the shelf");
+    console.log(index + " bottles of soda on the shelf");
+
+    console.log(
+        "Take one down, pass it around." + (index - 1) + " bottles of soda on the shelf"
+    );
+    console.log("");
+}
+
+console.log("No more bottles of soda on the shelf");
+
+
+ 
+console.log("Task: C");
+
+let gravity =
+const pi = 3,14;
+let peopleInARoom =
+
+
+
+console.log("Task: D");
+
+function addNumbers (num1, num2) {
+    return num1 + num2;
+}
+
+console.log("3 + 6 = " + add(3, 6));
+console.log("5673 + 234 = " + add(5673, 234));
+
+
+
+console.log("Task: E");
+
+for (let index = people.length - 1; index >= 0; index--) {
+    let person = people[index];
+    console.log(person);
+}
+
+
+
+console.log("Task: F");
+const phoneNumbers = ["110", "112", "113"];
+
+
+
+console.log("Task: G");
+console.log(phoneNumbers[phoneNumbers.length - 1]);
+
+
+console.log("Task: H");
+
+let peopleNames = [["Christian", "Simonsen"], ["Tony", "Bergholtz"]]
+for (let i = 0; i < peopleNames.length; i++) {
+    console.log(peopleNames[i][1]);
+  }
